3896a77b82e554ec52cf85d97746c3519689c568
Elia Shalish <elia.shalish@sap.com>
2019-06-15 23:55:20 -0400

    wro workaround

---
+++ custom/customstorefront/customstorefrontinitialdata/src/org/customstorefront/initialdata/constants/CustomstorefrontInitialDataConstants.java
@@ -0,0 +1,24 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2018 SAP SE or an SAP affiliate company.  All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package org.customstorefront.initialdata.constants;
+
+/**
+ * Global class for all CustomstorefrontInitialData constants.
+ */
+public final class CustomstorefrontInitialDataConstants extends GeneratedCustomstorefrontInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "customstorefrontinitialdata";
+
+	private CustomstorefrontInitialDataConstants()
+	{
+		//empty
+	}
+}
